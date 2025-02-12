e310a598105711309df37bd816dc279ab95ad5a8
BSeckett Stewart <bmste396@learner.dvusd.org>
2025-02-12 09:11:17 -0700

    car card game

---
+++ car card game/car card game/car card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_card_game
+{
+    internal class car_card
+    {
+        public int maxSpeed { get; set; }
+        public double zerotosixty { get; set; }
+
+        public int horsepower { get; set; }
+
+        public int cubiccentimeters {  get; set; }
+
+        public int cylinders { get; set; }
+
+        public int rpm { get; set; }
+
+    }
+}
